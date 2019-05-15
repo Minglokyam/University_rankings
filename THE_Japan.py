0339cb2bbd202eeb64d7a85e2a2f55e40931aa6d
Minglokyam <yamminglok@gmail.com>
2019-05-15 23:19:06 +0800

    Crawling code

---
+++ THE_Japan.py
@@ -0,0 +1,10 @@
+import json
+import requests
+
+url = "https://japanuniversityrankings.jp/data/get?from=0&to=500"
+r = requests.get(url)
+j = json.loads(r.text)
+
+with open("THE_Japan.csv", "w", encoding = "utf-8") as outfile:
+	for university in j:
+		outfile.write(str(university["total_rnk"]) + "," + university["jap_name"] + "," + university["total_score_v"] + "\n")
